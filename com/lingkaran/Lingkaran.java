b92ed36c7d899763a75783694e84ac151e2a335d
Yasirmubarok29 <114422610+Yasirmubarok29@users.noreply.github.com>
2022-11-15 22:04:45 +0700

    Add files via upload

---
+++ com/lingkaran/Lingkaran.java
@@ -0,0 +1,27 @@
+// Nama  : Yasir Mubarok
+// Kelas    : TI21E
+// Nim      : 20210040069
+package com.lingkaran;
+
+
+public class Lingkaran {
+    // Atribut
+    private int jejari;
+    private double luas;
+    
+    //Setter method untuk jejari
+    public void setJejari(int r){
+        if (r > 0 ){
+            this.jejari = r;
+        }
+        else{
+            this.jejari = 0;
+        }
+    }
+    
+    public double getLuas(){
+        this.luas = Math.PI * Math.pow(this.jejari, 2);
+        return this.luas;
+    }
+    
+}
